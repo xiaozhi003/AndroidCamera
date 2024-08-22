54c857ac548db5b9b14091c3270ee81181cdf936
zhi.wang <zhi.wang@opay-inc.com>
2024-08-22 15:52:01 +0800

    重构预览View

--- lib-camera/src/main/java/com/android/xz/view/Camera2SurfaceView.java
+++ lib-camera/src/main/java/com/android/xz/view/Camera2SurfaceView.java
@@ -1,74 +1,29 @@
 package com.android.xz.view;
 
 import android.content.Context;
-import android.graphics.PixelFormat;
-import android.os.Handler;
 import android.util.AttributeSet;
-import android.util.Size;
 import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 
 import com.android.xz.camera.Camera2Manager;
 import com.android.xz.camera.ICameraManager;
-import com.android.xz.camera.callback.CameraCallback;
 import com.android.xz.util.Logs;
+import com.android.xz.view.base.BaseSurfaceView;
 
 /**
- * Created by wangzhi on 2018/4/21.
+ * Created by wangzhi on 2024/8/22.
  */
-public class Camera2SurfaceView extends SurfaceView implements SurfaceHolder.Callback, CameraCallback {
-
-    private static final String TAG = Camera2SurfaceView.class.getSimpleName();
-    SurfaceHolder mSurfaceHolder;
-    private Context mContext;
-    private Handler mHandler;
-
-    private boolean hasSurface; // 是否存在摄像头显示层
-    private ICameraManager mCameraManager;
-    private int mRatioWidth = 0;
-    private int mRatioHeight = 0;
-    private int mSurfaceWidth;
-    private int mSurfaceHeight;
-    private Size mPreviewSize = new Size(0, 0);
+public class Camera2SurfaceView extends BaseSurfaceView {
 
     public Camera2SurfaceView(Context context) {
         super(context);
-        init(context);
     }
 
     public Camera2SurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
     }
 
     public Camera2SurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
-    private void init(Context context) {
-        mContext = context;
-        mHandler = new Handler(context.getMainLooper());
-        mSurfaceHolder = getHolder();
-        mSurfaceHolder.setFormat(PixelFormat.TRANSPARENT);//translucent半透明 transparent透明
-        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        mSurfaceHolder.addCallback(this);
-        mCameraManager = new Camera2Manager(context);
-        mCameraManager.setCameraCallback(this);
-    }
-
-    public ICameraManager getCameraManager() {
-        return mCameraManager;
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        Logs.i(TAG, "surfaceCreated..." + hasSurface);
-
-        if (!hasSurface && holder != null) {
-            hasSurface = true;
-            mCameraManager.openCamera();
-        }
     }
 
     @Override
@@ -84,90 +39,14 @@ public class Camera2SurfaceView extends SurfaceView implements SurfaceHolder.Cal
             ratio = width * 1.0f / height;
         }
         if (ratio == mPreviewSize.getHeight() * 1f / mPreviewSize.getWidth()) {
-            mCameraManager.startPreview(holder);
+            getCameraManager().startPreview(holder);
         }
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Logs.v(TAG, "surfaceDestroyed.");
-        closeCamera();
-        hasSurface = false;
-    }
-
-    public SurfaceHolder getSurfaceHolder() {
-        return mSurfaceHolder;
-    }
-
-    public void onResume() {
-        if (hasSurface) {
-            // 当activity暂停，但是并未停止的时候，surface仍然存在，所以 surfaceCreated()
-            // 并不会调用，需要在此处初始化摄像头
-            openCamera();
-        }
-    }
-
-    public void onPause() {
-        closeCamera();
-    }
-
-    /**
-     * 打开摄像头
-     */
-    private void openCamera() {
-        if (mSurfaceHolder == null) {
-            Logs.e(TAG, "SurfaceHolder is null.");
-            return;
-        }
-        if (mCameraManager.isOpen()) {
-            Logs.w(TAG, "Camera is opened！");
-            return;
-        }
-        mCameraManager.openCamera();
-    }
-
-    /**
-     * 关闭摄像头
-     */
-    private void closeCamera() {
-        mCameraManager.releaseCamera();
-    }
-
-    private String getString(int resId) {
-        return getResources().getString(resId);
-    }
-
-    private void setAspectRatio(int width, int height) {
-        if (width <= 0 || height <= 0) {
-            return;
-        }
-        Logs.i(TAG, "setAspectRatio: " + width + "x" + height);
-        mRatioWidth = width;
-        mRatioHeight = height;
-        requestLayout();
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        if (0 == mRatioWidth || 0 == mRatioHeight) {
-            setMeasuredDimension(width, height);
-        } else {
-            if (width < height * mRatioWidth / mRatioHeight) {
-                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
-            } else {
-                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
-            }
-        }
-        Logs.i(TAG, "onMeasure: " + getMeasuredWidth() + "x" + getMeasuredHeight());
     }
 
     @Override
     public void onOpen() {
         Logs.v(TAG, "onOpen.");
-        mPreviewSize = mCameraManager.getPreviewSize();
+        mPreviewSize = getCameraManager().getPreviewSize();
         getSurfaceHolder().setFixedSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
         float ratio;
         if (mSurfaceWidth > mSurfaceHeight) {
@@ -176,24 +55,12 @@ public class Camera2SurfaceView extends SurfaceView implements SurfaceHolder.Cal
             ratio = mSurfaceWidth * 1.0f / mSurfaceHeight;
         }
         if (ratio == mPreviewSize.getHeight() * 1f / mPreviewSize.getWidth()) {
-            mCameraManager.startPreview(getSurfaceHolder());
+            getCameraManager().startPreview(getSurfaceHolder());
         }
     }
 
     @Override
-    public void onOpenError(int error, String msg) {
-    }
-
-    @Override
-    public void onPreview(int previewWidth, int previewHeight) {
-        setAspectRatio(previewHeight, previewWidth);
-    }
-
-    @Override
-    public void onPreviewError(int error, String msg) {
-    }
-
-    @Override
-    public void onClose() {
+    public ICameraManager createCameraManager(Context context) {
+        return new Camera2Manager(context);
     }
 }
--- lib-camera/src/main/java/com/android/xz/view/Camera2TextureView.java
+++ lib-camera/src/main/java/com/android/xz/view/Camera2TextureView.java
@@ -1,214 +1,33 @@
 package com.android.xz.view;
 
 import android.content.Context;
-import android.graphics.SurfaceTexture;
-import android.os.Handler;
 import android.util.AttributeSet;
-import android.view.TextureView;
 
 import com.android.xz.camera.Camera2Manager;
-import com.android.xz.camera.CameraManager;
-import com.android.xz.camera.callback.CameraCallback;
-import com.android.xz.util.Logs;
+import com.android.xz.camera.ICameraManager;
+import com.android.xz.view.base.BaseTextureView;
 
 /**
- * Ø¬
- * 摄像头预览View
- * Created by xiaozhi on 2018/4/21.
+ * Created by wangzhi on 2024/8/22.
  */
-public class Camera2TextureView extends TextureView implements TextureView.SurfaceTextureListener, CameraCallback {
+public class Camera2TextureView extends BaseTextureView {
 
-    private static final String TAG = Camera2TextureView.class.getSimpleName();
-
-    private Context mContext;
-    private SurfaceTexture mSurfaceTexture;
-    private Handler mHandler;
-    private boolean isMirror;
-    private boolean hasSurface; // 是否存在摄像头显示层
-    private Camera2Manager mCameraManager;
-
-    private int mRatioWidth = 0;
-    private int mRatioHeight = 0;
-
-    private int mTextureWidth;
-    private int mTextureHeight;
 
     public Camera2TextureView(Context context) {
         super(context);
-        init(context);
     }
 
     public Camera2TextureView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
     }
 
     public Camera2TextureView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
-    }
-
-    private void init(Context context) {
-        mContext = context;
-        mHandler = new Handler(mContext.getMainLooper());
-        mCameraManager = new Camera2Manager(context);
-        mCameraManager.setCameraCallback(this);
-        setSurfaceTextureListener(this);
-    }
-
-    /**
-     * 获取摄像头工具类
-     *
-     * @return
-     */
-    public Camera2Manager getCameraManager() {
-        return mCameraManager;
-    }
-
-    /**
-     * 是否镜像
-     *
-     * @return
-     */
-    public boolean isMirror() {
-        return isMirror;
-    }
-
-    /**
-     * 设置是否镜像
-     *
-     * @param mirror
-     */
-    public void setMirror(boolean mirror) {
-        isMirror = mirror;
-        requestLayout();
-    }
-
-    private void setAspectRatio(int width, int height) {
-        if (width < 0 || height < 0) {
-            throw new IllegalArgumentException("Size cannot be negative.");
-        }
-        mRatioWidth = width;
-        mRatioHeight = height;
-        requestLayout();
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        if (0 == mRatioWidth || 0 == mRatioHeight) {
-            setMeasuredDimension(width, height);
-        } else {
-            if (width < height * mRatioWidth / mRatioHeight) {
-                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
-            } else {
-                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
-            }
-        }
-    }
-
-    /**
-     * 获取SurfaceTexture
-     *
-     * @return
-     */
-    @Override
-    public SurfaceTexture getSurfaceTexture() {
-        return mSurfaceTexture;
-    }
-
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
-        Logs.i(TAG, "onSurfaceTextureAvailable.");
-        mTextureWidth = width;
-        mTextureHeight = height;
-        mSurfaceTexture = surfaceTexture;
-        hasSurface = true;
-        openCamera();
-    }
-
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
-        Logs.i(TAG, "onSurfaceTextureSizeChanged.");
-    }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-        Logs.v(TAG, "onSurfaceTextureDestroyed.");
-        closeCamera();
-        hasSurface = false;
-        return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-    }
-
-    /**
-     * 打开摄像头并预览
-     */
-    public void onResume() {
-        if (hasSurface) {
-            // 当activity暂停，但是并未停止的时候，surface仍然存在，所以 surfaceCreated()
-            // 并不会调用，需要在此处初始化摄像头
-            openCamera();
-        }
-    }
-
-    /**
-     * 停止预览并关闭摄像头
-     */
-    public void onPause() {
-        closeCamera();
-    }
-
-    /**
-     * 初始化摄像头，较为关键的内容
-     */
-    private void openCamera() {
-        if (mSurfaceTexture == null) {
-            Logs.e(TAG, "mSurfaceTexture is null.");
-            return;
-        }
-        if (mCameraManager.isOpen()) {
-            Logs.w(TAG, "Camera is opened！");
-            return;
-        }
-        mCameraManager.openCamera();
-    }
-
-    private void closeCamera() {
-        mCameraManager.releaseCamera();
-    }
-
-    @Override
-    public void onOpen() {
-        mCameraManager.startPreview(mSurfaceTexture);
-    }
-
-    @Override
-    public void onOpenError(int error, String msg) {
-
-    }
-
-    @Override
-    public void onPreview(int previewWidth, int previewHeight) {
-        if (mTextureWidth > mTextureHeight) {
-            setAspectRatio(previewWidth, previewHeight);
-        } else {
-            setAspectRatio(previewHeight, previewWidth);
-        }
-    }
-
-    @Override
-    public void onPreviewError(int error, String msg) {
-
-    }
-
-    @Override
-    public void onClose() {
-
+    public ICameraManager createCameraManager(Context context) {
+        // 创建Camera2Manager
+        return new Camera2Manager(context);
     }
 }
